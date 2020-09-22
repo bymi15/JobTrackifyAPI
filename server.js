204f056df81108104f91db5b89fa5706e2ab85f9
unknown <bymi15@yahoo.com>
2020-09-23 00:25:37 +0100

    feat(server): add express mongoose boilerplate

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('./config');
+
+const app = express();
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
