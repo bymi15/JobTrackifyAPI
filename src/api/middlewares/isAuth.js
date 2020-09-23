8af3311e255b53e424a9aefa521ccb7d3a8daa5c
unknown <bymi15@yahoo.com>
2020-09-23 04:48:10 +0100

    feat(auth): implement user authentication with JWT

--- src/api/middlewares/isAuth.js
+++ src/api/middlewares/isAuth.js
@@ -20,7 +20,7 @@ const isAuth = jwt({
   secret: config.jwtSecret, // The _secret_ to sign the JWTs
   userProperty: 'token', // Use req.token to store the JWT
   getToken: getTokenFromHeader, // How to extract the JWT from the request
-  algorithms: ['RS256'], // Use the asymmetric RS256 (RSA Signature with SHA-256) algorithm
+  algorithms: ['HS256'], // Use the HS256 (HMAC with SHA-256) algorithm
 });
 
 export default isAuth;
--- src/loaders/logger.js
+++ src/loaders/logger.js
@@ -9,9 +9,9 @@ if (process.env.NODE_ENV !== 'development') {
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.cli(),
-        winston.format.splat(),
+        winston.format.splat()
       ),
-    }),
+    })
   );
 }
 
@@ -24,7 +24,7 @@ const LoggerInstance = winston.createLogger({
     }),
     winston.format.errors({ stack: true }),
     winston.format.splat(),
-    winston.format.json(),
+    winston.format.json()
   ),
   transports,
 });
