81c2b37b3ec236482e2fabc4209b8aaa1a6e33b4
unknown <bymi15@yahoo.com>
2020-09-23 01:19:51 +0100

    feat(models): add company model

---
+++ models/company.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const Company = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please enter the company name'],
+    },
+    logoUrl: String,
+    description: String,
+    website: String,
+    headquarters: String,
+    industry: String,
+    foundedYear: Number,
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model('Company', Company);
