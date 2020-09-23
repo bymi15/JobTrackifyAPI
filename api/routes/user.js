82da2714868404073a8d3edb1a850df16fb38eb5
unknown <bymi15@yahoo.com>
2020-09-23 02:42:29 +0100

    feat(routes): add user route

---
+++ api/routes/user.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { isAuth, attachCurrentUser } from '../middlewares';
+
+const route = Router();
+
+export default (app) => {
+  app.use('/user', route);
+  route.get('/me', isAuth, attachCurrentUser, (req, res) => {
+    return res.json({ user: req.currentUser }).status(200);
+  });
+};
