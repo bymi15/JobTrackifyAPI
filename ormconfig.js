d4e8195c210ae685fe209a965f82ce4e0472f44f
Brian Min <bymi15@yahoo.com>
2020-09-29 01:40:05 +0100

    refactor(ormconfig.js): add test mongodb connection

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,41 @@
-import config from './src/config';
+// import config from './src/config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require('./src/config').default;
 
-module.exports = {
-  type: 'mongodb',
-  url: config.databaseURL,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  synchronize: true,
-  logging: true,
-  entities: ['src/entity/**/*.ts'],
-  migrations: ['src/migration/**/*.ts'],
-  subscribers: ['src/subscriber/**/*.ts'],
-  cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber',
+module.exports = [
+  {
+    name: 'development',
+    type: 'mongodb',
+    url: config.databaseURL,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    synchronize: true,
+    logging: false,
+    entities: ['src/entity/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber',
+    },
   },
-};
+  {
+    name: 'test',
+    type: 'mongodb',
+    url: config.testDatabaseURL,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    synchronize: true,
+    logging: false,
+    dropSchema: true,
+    entities: ['src/entity/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber',
+    },
+  },
+];
