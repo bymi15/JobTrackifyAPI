fd124a1d98eae067bd058529596b729f92be03c5
Brian Min <bymi15@yahoo.com>
2020-10-01 03:53:34 +0100

    feat(factories): add userFactory function for saving test user data to db

---
+++ src/factories/userFactory.ts
@@ -0,0 +1,25 @@
+import { User, Role } from '../entities/User';
+import { getConnection } from 'typeorm';
+import bcrypt from 'bcrypt';
+import * as faker from 'faker';
+
+export default async (data?: User): Promise<User> => {
+  let password = await bcrypt.hash(faker.random.word(), 12);
+  if (data && data.password) {
+    password = await bcrypt.hash(data.password, 12);
+  }
+  const randomRole: Role = faker.random.arrayElement([
+    'user',
+    'staff',
+    'admin',
+  ]);
+  const user = new User({
+    firstName: (data && data.firstName) || faker.name.firstName(),
+    lastName: (data && data.lastName) || faker.name.lastName(),
+    email: (data && data.email) || faker.internet.email(),
+    password,
+    role: (data && data.role) || randomRole,
+  });
+  await getConnection().getMongoRepository(User).save(user);
+  return user;
+};
