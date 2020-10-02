4847d5e4dc42757209c5fa77e4f3b12373f8b9d3
Brian Min <bymi15@yahoo.com>
2020-10-02 13:12:40 +0100

    feat(config): add development and production db

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,9 +4,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 dotenv.config();
 
-let databaseUrl = process.env.MONGODB_URI;
+let databaseUrl = process.env.MONGODB_DEV_URI;
 if (process.env.NODE_ENV === 'test') {
   databaseUrl = process.env.MONGODB_TEST_URI;
+} else if (process.env.NODE_ENV === 'production') {
+  databaseUrl = process.env.MONGODB_URI;
 }
 
 export default {
