36e8813f90a150560739e69573e86667a60bfe1d
Brian Min <bymi15@yahoo.com>
2020-10-06 13:28:38 +0100

    feat(Board): add Board API
    BREAKING CHANGE: add Board entity, route, service and factory

---
+++ src/api/entities/Board.ts
@@ -0,0 +1,33 @@
+import { Entity, ObjectIdColumn, Column, ObjectID } from 'typeorm';
+import { IsString, IsNotEmpty } from 'class-validator';
+import { User } from './User';
+
+@Entity()
+export class Board {
+  @ObjectIdColumn()
+  id?: ObjectID;
+
+  @Column()
+  @IsString()
+  title?: string;
+
+  @Column()
+  @IsNotEmpty()
+  owner?: ObjectID | User;
+
+  @Column()
+  @IsString()
+  createdAt?: string = new Date().toISOString();
+
+  @Column()
+  @IsString()
+  updatedAt?: string = new Date().toISOString();
+
+  public constructor(data?: Board) {
+    if (data) {
+      this.title = data.title;
+      this.owner = data.owner;
+      this.updatedAt = new Date().toISOString();
+    }
+  }
+}
