0f3a48cc2437f0a7e8495a382111c823da0967ba
Brian Min <bymi15@yahoo.com>
2020-10-07 03:55:06 +0100

    feat(factories): add Board factory

--- src/database/factories/BoardFactory.ts
+++ src/database/factories/BoardFactory.ts
@@ -1,11 +1,12 @@
 import { Board } from '../../api/entities/Board';
 import * as faker from 'faker';
+import { ObjectId } from 'mongodb';
 import { ObjectID } from 'typeorm';
 
 export default (data?: Board): Board => {
   const board = new Board({
     title: (data && data.title) || faker.random.word(),
-    owner: (data && data.owner) || new ObjectID(faker.random.word()),
+    owner: (data && data.owner) || (new ObjectId() as ObjectID),
   });
   return board;
 };
