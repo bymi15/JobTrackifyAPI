9bc3c98f49f147383ef133e21c98cbaea2ddde46
Brian Min <bymi15@yahoo.com>
2020-11-09 01:54:15 +0000

    chore(dependencyInjector): load nodemailer transporter object to container

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -1,9 +1,25 @@
 import { Container } from 'typedi';
 import Logger from '../logger';
+import nodemailer from 'nodemailer';
+import config from '../config';
 
 export default (): void => {
   try {
     Container.set('logger', Logger);
+    Container.set(
+      'transporter',
+      nodemailer.createTransport(
+        {
+          host: config.emailHost,
+          port: 587,
+          auth: {
+            user: config.emailUser,
+            pass: config.emailPass,
+          },
+        },
+        { secure: true }
+      )
+    );
   } catch (e) {
     Logger.error('Error on dependency injector loader: %o', e);
     throw e;
